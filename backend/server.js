2807d9bfdef3b3891a49dd5925fe6bd227230c4d
Thomas <tholmes59@gmail.com>
2023-01-04 16:31:33 -0800

    add get request to route api/tickets and return a 200 status when hitting the route

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,8 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.get("/api/tickers", (req, res) => {
+  res.status(200).json({ message: "The tickers!" });
+});
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
