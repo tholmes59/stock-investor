da3adced094e542cb1abd5aec664ee749280961a
Thomas <tholmes59@gmail.com>
2023-01-06 17:01:18 -0800

    allow error message to be read in json and import and use error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/tickers", require("./routes/tickerRoutes"));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
