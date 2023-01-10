16e58f4fd57b8cfafe4a378cffb90495c6f10273
Thomas <tholmes59@gmail.com>
2023-01-09 16:36:08 -0800

    import async handler and wrap controller functions in them in order to not habe to use .then chaining

--- backend/controllers/tickerController.js
+++ backend/controllers/tickerController.js
@@ -1,35 +1,37 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc     Get tickers
 //@route    GET /api/tickers
 //@access   Private
-const getTickers = (req, res) => {
+const getTickers = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "The tickers!" });
-};
+});
 
 //@desc     Set tickers
 //@route    POST /api/tickers
 //@access   Private
-const setTicker = (req, res) => {
+const setTicker = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a ticker");
   }
 
   res.status(200).json({ message: "Set ticker!" });
-};
+});
 
 //@desc     Edit tickers
 //@route    PUT /api/tickers
 //@access   Private
-const editTicker = (req, res) => {
+const editTicker = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update ticker ${req.params.id}` });
-};
+});
 
 //@desc     Delete tickers
 //@route    DELETE /api/tickers
 //@access   Private
-const deleteTicker = (req, res) => {
+const deleteTicker = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete ticker ${req.params.id}` });
-};
+});
 
 module.exports = {
   getTickers,
