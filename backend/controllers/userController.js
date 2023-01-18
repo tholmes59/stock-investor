47b3e59881cef6f9203df423366adfdfbdc44dae
Thomas <tholmes59@gmail.com>
2023-01-18 11:21:01 -0800

    add functions to user controller to login user and to get user information

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,26 @@
+//@desc     Register new user
+//@route    POST /api/users
+//@access   Public
 const registerUser = (req, res) => {
   res.status(200).json({ message: "User created" });
 };
 
+//@desc     Authenticate new user
+//@route    POST /api/login
+//@access   Public
+const loginUser = (req, res) => {
+  res.status(200).json({ message: "Login user" });
+};
+
+//@desc     Get user data
+//@route    GET /api/users
+//@access   Public
+const getUser = (req, res) => {
+  res.status(200).json({ message: "Display user info" });
+};
+
 module.exports = {
   registerUser,
+  loginUser,
+  getUser,
 };
