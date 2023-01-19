ac8dea86d00df880358a1fa504122952c5f2bfc3
Thomas <tholmes59@gmail.com>
2023-01-19 10:54:05 -0800

    update login function in user controller to login user with email and password using bcrypt compare function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -46,7 +46,21 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route    POST /api/login
 //@access   Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Login user" });
+  const { email, password } = req.body;
+
+  // Check for user email
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User credentials");
+  }
 });
 
 //@desc     Get user data
