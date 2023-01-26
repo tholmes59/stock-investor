e561222f99dd47814278a0a01bf1958604ccff17
Thomas <tholmes59@gmail.com>
2023-01-26 12:56:20 -0800

    add styling to header component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,27 +1,37 @@
 import React from "react";
-import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { FaSignInAlt, FaSignOutAlt, FaUser, FaHome } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import logo from "../assets/investorInfoLogo2.png";
 
 export default function Header() {
   return (
-    <header>
-      <div>
-        <Link to="/">Home</Link>
+    <header className="relative h-auto w-full p-2 bg-[#003049] text-[#edf2f5]">
+      <div className="flex flex-row justify-between text-center px-2 text-[#edf2f5]">
+        <div>
+          <Link to="/">
+            <img src={logo} alt="InvestorInfo" className="mx-10 w-36" />
+          </Link>
+        </div>
+        <ul className="flex flex-row justify-between items-center">
+          <li className="ml-5 hover:text-[#f77f00] hover:cursor-pointer">
+            <Link to="/" className="flex items-center">
+              <FaHome className="mr-1.5" />
+              Home
+            </Link>
+          </li>
+          <li className="ml-5 hover:text-[#f77f00] hover:cursor-pointer">
+            <Link to="/login" className="flex items-center">
+              <FaSignInAlt className="mr-1.5" /> Login
+            </Link>
+          </li>
+          <li className="ml-5 hover:text-[#f77f00] hover:cursor-pointer">
+            <Link to="/register" className="flex items-center">
+              <FaUser className="mr-1.5" />
+              Sign-up
+            </Link>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>
-          <Link to="/login">
-            <FaSignInAlt />
-            Login
-          </Link>
-        </li>
-        <li>
-          <Link to="/register">
-            <FaUser />
-            Sign-up
-          </Link>
-        </li>
-      </ul>
     </header>
   );
 }
