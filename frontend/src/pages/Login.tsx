23212ec9601ee9e932fa83a906bb4af4928b68fe
Thomas <tholmes59@gmail.com>
2023-01-27 11:08:02 -0800

    build out structure and styling of login page and add onchange function and shell of onsubmit function

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { FaSignInAlt } from "react-icons/fa";
 
 export default function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex flex-col mt-20 mb-16 mx-auto items-center w-full max-w-2xl border p-6 rounded-md bg-[#f0eff4] shadow-md">
+      <section className="flex flex-col items-center text-2xl font-medium mb-12 mt-12 p-4">
+        <h1 className="flex items-center text-5xl mb-6 text-[#003049]">
+          <FaSignInAlt className="flex items-center align-middle mr-4" />
+          Login
+        </h1>
+        <p className="text-5xl text-[#003049]">Welcome Back!</p>
+      </section>
+      <section className="w-full mb-16">
+        <form onSubmit={onSubmit} className="w-7/10 mx-auto">
+          <div>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter email"
+              onChange={onChange}
+              className="w-full p-2.5 border border-gray-300 rounded-md mb-4 font-inherit"
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter password"
+              onChange={onChange}
+              className="w-full p-2.5 border border-gray-300 rounded-md mb-4 font-inherit"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="py-3 px-4 w-full rounded-md text-white text-center cursor-pointer flex items-center justify-center bg-[#003049] border-[#003049] hover:text-[#f77f00]"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
     </div>
-  )
+  );
 }
