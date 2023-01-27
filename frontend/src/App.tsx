462f334893ce105fedd08e8c957557dacc37efed
Thomas <tholmes59@gmail.com>
2023-01-26 16:57:11 -0800

    import search bars into app.tsx and style the search container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import SearchCompanyProfile from "./components/SearchCompanyProfile";
+import SearchCompanyTicker from "./components/SearchCompanyTicker";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -11,6 +13,10 @@ function App() {
       <Router>
         <div>
           <Header />
+          <section className="flex justify-between py-8 px-16">
+            <SearchCompanyProfile />
+            <SearchCompanyTicker />
+          </section>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
