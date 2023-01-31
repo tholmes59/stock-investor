ab3a4660a4386b735032565111f28d863c09ec4c
Thomas <tholmes59@gmail.com>
2023-01-30 17:58:49 -0800

    update union type for user data to include object

--- frontend/src/features/auth/authService.ts
+++ frontend/src/features/auth/authService.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL = "/api/users/";
 
 //Register user
-const register = async (userData: string | null | unknown) => {
+const register = async (userData: {} | null | unknown) => {
   const response = await axios.post(API_URL, userData);
 
   if (response.data) {
