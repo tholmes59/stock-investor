2ae69fb3f958c184acfd8703fd703bd041028ce9
Thomas <tholmes59@gmail.com>
2023-01-31 16:12:41 -0800

    add ternary to display logout button if user and create onlogout function to logout, dispatch, and navigate to home page on click

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,20 @@
-import React from "react";
 import { FaSignInAlt, FaSignOutAlt, FaUser, FaHome } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/investorInfoLogo2.png";
+import { useAppSelector, useAppDispatch } from "../app/hooks";
+import { logout, reset } from "../features/auth/authSlice";
 
 export default function Header() {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const { user } = useAppSelector((state) => state.auth);
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/");
+  };
+
   return (
     <header className="relative h-auto w-full p-2 bg-[#003049] text-[#edf2f5]">
       <div className="flex flex-row justify-between text-center px-2 text-[#edf2f5]">
@@ -19,17 +30,27 @@ export default function Header() {
               Home
             </Link>
           </li>
-          <li className="ml-5 hover:text-[#f77f00] hover:cursor-pointer">
-            <Link to="/login" className="flex items-center">
-              <FaSignInAlt className="mr-1.5" /> Login
-            </Link>
-          </li>
-          <li className="ml-5 hover:text-[#f77f00] hover:cursor-pointer">
-            <Link to="/register" className="flex items-center">
-              <FaUser className="mr-1.5" />
-              Sign-up
-            </Link>
-          </li>
+          {user ? (
+            <li className="ml-5 hover:text-[#f77f00] hover:cursor-pointer">
+              <button onClick={onLogout} className="flex items-center">
+                <FaSignOutAlt className="mr-1.5" /> Logout
+              </button>
+            </li>
+          ) : (
+            <>
+              <li className="ml-5 hover:text-[#f77f00] hover:cursor-pointer">
+                <Link to="/login" className="flex items-center">
+                  <FaSignInAlt className="mr-1.5" /> Login
+                </Link>
+              </li>
+              <li className="ml-5 hover:text-[#f77f00] hover:cursor-pointer">
+                <Link to="/register" className="flex items-center">
+                  <FaUser className="mr-1.5" />
+                  Sign-up
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </header>
