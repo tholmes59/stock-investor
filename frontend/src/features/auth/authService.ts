bec4c0c678dd2e94bce63925d610fc72d4b06c21
Thomas <tholmes59@gmail.com>
2023-02-03 14:14:50 -0800

    create auth service to post user data to database and login user via setting user item in local storage

--- frontend/src/features/auth/authService.ts
+++ frontend/src/features/auth/authService.ts
@@ -12,6 +12,16 @@ const register = async (userData: {} | null | unknown) => {
   return response.data;
 };
 
+//Login user
+const login = async (userData: {} | null | unknown) => {
+  const response = await axios.post(API_URL + "login", userData);
+
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+  return response.data;
+};
+
 //Logout user
 const logout = () => {
   localStorage.removeItem("user");
@@ -19,6 +29,7 @@ const logout = () => {
 
 const authService = {
   register,
+  login,
   logout,
 };
 
