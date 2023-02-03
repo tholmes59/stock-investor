86194d2f8b91f5a017a92654c98b4ec3c05f7482
Thomas <tholmes59@gmail.com>
2023-02-03 14:40:18 -0800

    create component to fetch FTSE index returns from financial model prep and display them

---
+++ frontend/src/components/Index Returns/Indicies/Ftse.tsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+
+function Ftse() {
+  const [ftse, setFtse] = useState<any>();
+
+  useEffect(() => {
+    const loadFtse = async () => {
+      const ftse = await fetch(
+        `https://financialmodelingprep.com/api/v3/profile/VGK?apikey=${process.env.REACT_APP_FMP_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => data);
+      console.log(ftse);
+      setFtse(ftse);
+    };
+    loadFtse();
+  }, []);
+
+  let price =
+    ftse &&
+    ftse
+      .map((x: any) => x.price)
+      .shift()
+      .toFixed(2) * 1;
+  let priceChange =
+    ftse &&
+    ftse
+      .map((x: any) => x.changes)
+      .shift()
+      .toFixed(2) * 1;
+  let percentChange =
+    (((price + priceChange) / price - 1) * 100).toFixed(2) + "%";
+
+  let pricePercentageFontColor = {};
+  if (priceChange >= 0) {
+    pricePercentageFontColor = { color: "green" };
+  }
+  if (((price + priceChange) / price - 1) * 100 < 0) {
+    pricePercentageFontColor = { color: "red" };
+  }
+
+  return (
+    <>
+      <div className="flex flex-col basis-8 p-2 border border-gray-300 rounded-lg whitespace-nowrap w-full text-sm">
+        <div className="font-bold">FTSE</div>
+        <div className="index-price">
+          {ftse &&
+            ftse
+              .map((x: any) => x.price)
+              .shift()
+              .toFixed(2)}
+        </div>
+        <div className="flex">
+          <div className="pr-1.5" style={pricePercentageFontColor}>
+            {priceChange}
+          </div>
+          <div
+            className="index-percent-change"
+            style={pricePercentageFontColor}
+          >
+            {percentChange}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Ftse;
