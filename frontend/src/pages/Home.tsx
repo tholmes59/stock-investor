5b571d793c2556aa79435b29ad2c28e112be5e1d
Thomas <tholmes59@gmail.com>
2023-02-03 15:09:59 -0800

    import Indicies, sectors and top news components to be displayed on home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Sectors from "../components/Home/Sectors";
+import TopNews from "../components/Home/TopNews";
+import Indicies from "../components/Index Returns/Indicies";
+import dayjs from "dayjs";
 
 export default function Home() {
+  const date = dayjs().format("MMMM D, YYYY h:m:ss a");
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
+    <>
+      <h1 className="text-left pl-4 mb-2 text-2xl">Today in the Markets</h1>
+      <p className="text-left pl-4 m-0 italic">As of {date}</p>
+      <div className="grid grid-cols-[repeat(2,1fr)] gap-10 p-4">
+        <Indicies />
+        <Sectors />
+        <TopNews />
+      </div>
+    </>
+  );
 }
