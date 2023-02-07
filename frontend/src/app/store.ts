d53c31fcbcbc40d43a42728592aba9965cbd217f
Thomas <tholmes59@gmail.com>
2023-02-07 10:58:11 -0800

    add stock reducer to redux store

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
+import stockReducer from "../features/stock/stockSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    stock: stockReducer,
   },
 });
 
