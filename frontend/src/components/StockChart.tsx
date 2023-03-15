0118c6a5139c4a40e2228a1fe522f8d7321aa27d
Thomas <tholmes59@gmail.com>
2023-03-14 17:26:32 -0700

    create stock chart component by passing price props and displaying stock chart from react highcharts

---
+++ frontend/src/components/StockChart.tsx
@@ -0,0 +1,90 @@
+import React, { useRef } from "react";
+import Highcharts from "highcharts/highstock";
+import HighchartsReact from "highcharts-react-official";
+
+const StockChart = ({ price }: HighchartsReact.Props) => {
+  const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
+
+  let chartName = price && price.symbol;
+  let dataArray = [];
+
+  let priceArray =
+    price && Array.isArray(price.historical) ? price.historical : [];
+
+  let priceData = price && [...priceArray];
+
+  let stockData = priceData.sort(function compare(a: any, b: any) {
+    let dateA: any = new Date(a.date);
+    let dateB: any = new Date(b.date);
+    return dateA - dateB;
+  });
+
+  if (stockData) {
+    for (let i = 0; i < stockData.length; i++) {
+      let tempArray = [];
+      tempArray.push(new Date(stockData[i].date).getTime(), stockData[i].close);
+      dataArray.push(tempArray);
+      tempArray = [];
+    }
+  }
+
+  const options: Highcharts.Options = {
+    title: {
+      text: chartName + " Chart",
+    },
+    series: [
+      {
+        type: "line",
+        data: dataArray,
+      },
+    ],
+    rangeSelector: {
+      buttons: [
+        {
+          type: "day",
+          count: 7,
+          text: "7d",
+        },
+        {
+          type: "month",
+          count: 1,
+          text: "1m",
+        },
+        {
+          type: "month",
+          count: 3,
+          text: "3m",
+        },
+        {
+          type: "ytd",
+          text: "YTD",
+        },
+        {
+          type: "year",
+          count: 1,
+          text: "1yr",
+        },
+        {
+          type: "all",
+          text: "All",
+        },
+      ],
+      selected: 5,
+    },
+  };
+
+  return (
+    <div>
+      {price && (
+        <HighchartsReact
+          options={options}
+          ref={chartComponentRef}
+          highcharts={Highcharts}
+          constructorType={"stockChart"}
+        />
+      )}
+    </div>
+  );
+};
+
+export default StockChart;
