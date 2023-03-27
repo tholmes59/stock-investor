ed1e777b11bda86726f5647ac8117d13c29764db
Thomas <tholmes59@gmail.com>
2023-03-27 15:56:21 -0700

    edit styling on news card

--- frontend/src/components/NewsCard.tsx
+++ frontend/src/components/NewsCard.tsx
@@ -2,17 +2,21 @@ import React from "react";
 
 const NewsCard = ({ news }: any) => {
   return (
-    <div className="news-card-container">
-      <div className="news-image-container">
-        <img alt="CompanyLogo" src={news.urlToImage}></img>
+    <div className="flex flex-row justify-center items-center text-[0.8rem] mx-0 my-2.5 border-t-[lightgray] border-t-[1px] border-solid">
+      <div className="w-[155px] h-[100px] mr-5">
+        <img
+          alt="CompanyLogo"
+          src={news.urlToImage}
+          className="w-[155px] h-[100px] object-cover mx-2.5 my-0"
+        ></img>
       </div>
-      <div className="news-container">
+      <div className="flex flex-col mx-0 my-2.5 text-left">
         <p>
           <a
             href={news.url}
             target="_blank"
             rel="noopener noreferrer"
-            className="news-title"
+            className="no-underline text-[black] text-[1.1rem] font-[bold] hover:text-[#0f69ff]"
           >
             {news.title}
           </a>
