8e174bfa868cf6e26030a32646fc562affc328e5
Thomas <tholmes59@gmail.com>
2023-03-29 11:46:49 -0700

    change name of stock dashboard page to user dashboard

--- frontend/src/pages/StockDashboard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function StockDashboard() {
-  return (
-    <div>
-      <h1>Stock Dashbaord</h1>
-    </div>
-  )
-}
---
+++ frontend/src/pages/UserDashboard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function UserDashboard() {
+  return (
+    <div>
+      <h1>User Dashbaord</h1>
+    </div>
+  );
+}
