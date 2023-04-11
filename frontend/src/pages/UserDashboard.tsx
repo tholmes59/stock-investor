a85f12592f1a6af902186e5dcc630b34099ef838
Thomas <tholmes59@gmail.com>
2023-04-11 10:23:48 -0700

    import user data state from redux and display user name

--- frontend/src/pages/UserDashboard.tsx
+++ frontend/src/pages/UserDashboard.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useAppSelector, useAppDispatch } from "../app/hooks";
 
 export default function UserDashboard() {
+  const { user, isLoading, isSuccess, isError, message } = useAppSelector(
+    (state) => state.auth
+  );
+
+  const { userID } = useParams<{ userID: string }>();
+  const dispatch = useAppDispatch();
+
+  interface User {
+    _id: string;
+    name: string;
+    email: string;
+    token: string;
+  }
+
+  console.log(user);
+
   return (
     <div>
-      <h1>User Dashbaord</h1>
+      <h1>{user && (user as User).name} Portfolio</h1>
     </div>
   );
 }
