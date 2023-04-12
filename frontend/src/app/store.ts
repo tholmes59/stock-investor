8e848bf4bf5a5895304ab517b44083beec1f502d
Thomas <tholmes59@gmail.com>
2023-04-12 15:56:35 -0700

    add ticker reducer to redux store

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import stockReducer from "../features/stock/stockSlice";
+import tickerReducer from "../features/ticker/tickerSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     stock: stockReducer,
+    ticker: tickerReducer,
   },
 });
 
