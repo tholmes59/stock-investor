f741020ee549fbec9af512d2c7a9cdd3d44f5c93
Thomas <tholmes59@gmail.com>
2023-04-25 14:43:23 -0700

    implement set timeout to display loading spinner while page loads

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sectors from "../components/Home/Sectors";
 import TopNews from "../components/Home/TopNews";
 import Indicies from "../components/Index Returns/Indicies";
 import dayjs from "dayjs";
+import Spinner from "../components/Spinner";
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   const date = dayjs().format("MMMM D, YYYY h:m:ss a");
+
+  if (loading) return <Spinner />;
+
   return (
     <>
       <h1 className="text-left pl-4 mb-2 text-2xl">Today in the Markets</h1>
