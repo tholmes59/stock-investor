03473790890fc40c85e3fca852d31a85a7833720
Thomas <tholmes59@gmail.com>
2023-04-28 10:09:12 -0700

    build out ticker card and add styling

--- frontend/src/components/TickerCard.tsx
+++ frontend/src/components/TickerCard.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { MdDeleteForever } from "react-icons/md";
 
 function TickerCard({ ticker }: any) {
   console.log(ticker);
-  return <div>{ticker.companyName}</div>;
+
+  const handleSubmit = (e: React.SyntheticEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    console.log("Delete");
+  };
+  return (
+    <div className="flex flex-row items-center m-12 py-6 justify-around shadow-md rounded-md bg-slate-50">
+      <img src={ticker.image} alt="company logo" className="h-12"></img>
+      <div className="">{ticker.symbol}</div>
+      <div className="w-52">{ticker.companyName}</div>
+      <button onClick={handleSubmit}>
+        <MdDeleteForever size={20} />
+      </button>
+    </div>
+  );
 }
 
 export default TickerCard;
