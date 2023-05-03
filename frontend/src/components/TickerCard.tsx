ead3edd757c197dadc1b0628954f899d5bdff9ed
Thomas <tholmes59@gmail.com>
2023-05-02 17:27:09 -0700

    add onclick event to navigate to stock display page when clicking the ticker

--- frontend/src/components/TickerCard.tsx
+++ frontend/src/components/TickerCard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { MdDeleteForever } from "react-icons/md";
 import { useAppDispatch } from "../app/hooks";
 import { deleteTicker, reset } from "../features/ticker/tickerSlice";
+import { useNavigate } from "react-router-dom";
 
 interface TickerCardProps {
   ticker: any;
@@ -10,15 +11,24 @@ interface TickerCardProps {
 
 function TickerCard({ ticker, onDeleteTicker }: TickerCardProps) {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   const handleSubmit = () => {
     dispatch(deleteTicker(ticker._id));
     onDeleteTicker(ticker._id);
   };
 
+  const handleClick = () => {
+    console.log("clicked");
+    navigate(`/stock/${ticker.symbol}`);
+  };
+
   return (
     <div className="flex flex-row items-center">
-      <div className="flex flex-row items-center my-8 ml-8 py-6 justify-around shadow-md rounded-md bg-slate-50 w-10/12 hover:scale-105">
+      <div
+        onClick={handleClick}
+        className="flex flex-row items-center my-8 ml-8 py-6 justify-around shadow-md rounded-md bg-slate-50 w-10/12 hover:scale-105"
+      >
         <img src={ticker.image} alt="company logo" className="h-12"></img>
         <div className="">{ticker.symbol}</div>
         <div className="w-52">{ticker.companyName}</div>
