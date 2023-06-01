89e95b6ef82853aede403c06ced9a1f4c6f079a2
Thomas <tholmes59@gmail.com>
2023-06-01 09:32:03 -0700

    add nasdaq to indicies and improve layout

--- frontend/src/components/Index Returns/Indicies.tsx
+++ frontend/src/components/Index Returns/Indicies.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import Sp500 from "./Indicies/Sp500";
 import Ftse from "./Indicies/Ftse";
 import Nikk from "./Indicies/Nikk";
+import Nasdaq from "./Indicies/Nasdaq";
 
 const Indicies = () => {
   return (
-    <div className="flex flex-row flex-wrap gap-2.5 content-start">
+    // <div className="flex flex-row flex-wrap gap-2.5 content-start">
+    <div className="grid grid-cols-2 gap-2.5 ">
       <Sp500 />
+      <Nasdaq />
       <Ftse />
       <Nikk />
     </div>
